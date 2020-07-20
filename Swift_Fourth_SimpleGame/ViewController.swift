10f09acd385164f5b79fedfb5b26e426c09181de
Onuri_kjs <rmawltjs0812@naver.com>
2020-07-20 15:25:54 +0900

    Initial Commit

---
+++ Swift_Fourth_SimpleGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_Fourth_SimpleGame
+//
+//  Created by active on 2020/07/20.
+//  Copyright © 2020 activeKJS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
